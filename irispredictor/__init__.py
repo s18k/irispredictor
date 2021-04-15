6df15c828d5be9598ef2228275b3cd6ba809418c
Shreyas <shreyaskamath18@gmail.com>
2021-04-15 18:44:33 +0530

    Initial Commit

---
+++ irispredictor/__init__.py
@@ -0,0 +1,32 @@
+from flask import Flask,render_template,request
+from sklearn.datasets import load_iris
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.model_selection import train_test_split
+import pickle
+import numpy as np
+app = Flask(__name__)
+
+import iris-predictor.views
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ setup.py
@@ -0,0 +1,38 @@
+import re
+from setuptools import setup
+
+version = '0.0.1'
+
+
+def parse_requirements(file_name):
+    requirements = []
+    for line in open(file_name, 'r').read().split('\n'):
+        if re.match(r'(\s*#)|(\s*$)', line):
+            continue
+        if re.match(r'\s*-e\s+', line):
+            requirements.append(re.sub(r'\s*-e\s+.*#egg=(.*)$', r'\1', line))
+        elif re.match(r'\s*-f\s+', line):
+            pass
+        else:
+            requirements.append(line)
+
+    return requirements
+
+package_name='irispredictor'
+base_url = 'https://github.com/s18k/irispredictor/'
+setup(
+    name='irispredictor',
+    version=version,
+    description='A simple Flask application for Iris prediction',
+    author='Shreyas Kamath',
+    author_email='shreyaskamath18@gmail.com',
+    license='MIT',
+    url='https://github.com/s18k/irispredictor',
+    
+	download_url='{0}/archive/{1}-{2}'.format(base_url, package_name,version),
+    packages=['iris-predictor'],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=parse_requirements("requirements.txt"),
+   
+)
